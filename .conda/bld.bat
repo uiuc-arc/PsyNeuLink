ece2f54d63d3fb39fdf0f43d9f52794de4945687
Dillon Smith <dillon.th.smith@gmail.com>
2019-11-13 12:28:35 -0500

    configuring conda build

--- .conda/bld.bat
+++ .conda/bld.bat
@@ -13,7 +13,7 @@ rem # this out myself, ughhh.
 set PIP_NO_INDEX=False
 set PIP_NO_DEPENDENCIES=False
 set PIP_IGNORE_INSTALLED=False
-pip install cattrs==1.0.0rc opencv-python-headless==3.4.1.15 PySide2==5.12.0 imgaug qimage2ndarray==1.8 imgstore
+pip install typecheck-decorator==1.2 toposort==1.4
 
 rem # Use and update environment.yml call to install pip dependencies. This is slick.
 rem # While environment.yml contains the non pip dependencies, the only thing left
--- .conda/build.sh
+++ .conda/build.sh
@@ -7,4 +7,17 @@
 # all get installed twice if we do this. pip doesn't see the
 # conda install of the packages.
 
+# Install the pip dependencies and their dependencies. Conda turns of
+# pip index and dependencies by default so re-enable them. Had to figure
+# this out myself, ughhh.
 export PIP_NO_INDEX=False
+export PIP_NO_DEPENDENCIES=False
+export PIP_IGNORE_INSTALLED=False
+pip install typecheck-decorator==1.2 toposort==1.4
+
+# Install requires setuptools-scm
+pip install setuptools-scm
+
+# Install psyneulink itself.
+# NOTE: This is the recommended way to install packages
+python setup.py install --single-version-externally-managed --record=record.txt
